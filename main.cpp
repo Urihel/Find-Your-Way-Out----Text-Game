c0a1fac6c928ff982d6e044defef9fa751a4e6c6
Urihel Coleman <45701213+Urihel@users.noreply.github.com>
2019-01-18 11:49:00 -0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -25,8 +25,18 @@ void gameText() {
     cout<<"To start a new game please select from the numbers below."<<endl;
 }
 
+void entrance() {
+    cout<<"\nYou found your way into an abandoned warehouse."<<endl;
+    cout<<"Right in front of you is a hallway which leads \nstraight ahead, a staircase to 2nd floor on the \nright and a door to a room to left"<<endl;
+    cout<<"\nType a number with keypad to choose path"<<endl;
+    cout<<"1.\tGo down hallway"<<endl;
+    cout<<"2.\tTake staircase to second floor"<<endl;
+    cout<<"3.\tOpen door on the left"<<endl;
+}
+//main body-------------------------------------------------------------------------------
 int main () {
     gameText();
     startGame();
+    entrance();
 }
 
