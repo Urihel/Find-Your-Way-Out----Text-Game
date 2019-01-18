1d2279e0a6aa5da26cf64a89d05da4dd0a885978
Urihel Coleman <45701213+Urihel@users.noreply.github.com>
2019-01-18 13:08:55 -0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -7,6 +7,7 @@ using namespace std;
 void gameMenu() {
     cout<<"\n1.\tNew Game"<<endl;
 }
+//--------------------------------------
 void startGame() {
     gameMenu();
 
@@ -18,13 +19,14 @@ void startGame() {
         cout<<"starting game"<<endl;
     }
 }
+//--------------------------------------
 void gameText() {
     cout<<"\'Find Your Way Out\'--\'Text Based Game\'";
     cout<<"\n"<<endl;
     cout<<"\nFind Your Way Out, is a Text based game that uses your keypad on your keyboard to enter in and out of rooms."<<endl;
     cout<<"To start a new game please select from the numbers below."<<endl;
 }
-
+//--------------------------------------
 void entrance() {
     cout<<"\nYou found your way into an abandoned house."<<endl;
     cout<<"Right in front of you is a hallway which leads \nstraight ahead, a staircase to 2nd floor on the \nright and a door to a room to left"<<endl;
@@ -50,14 +52,29 @@ void entrance() {
 
 
 }
+//------------------------------------------------
 void hallway() {
     cout<<"\nYou walk down the hallway and see a table in the middle with a lighter, fuel, and club wrapped in cloth"<<endl;
-    cout<<"Under the table there is a rug, to left is storage room which has a door on it. and on the right is a small kitchen"<<endl;
+    cout<<"Under the table there is a rug, on top of the table is a lighter, fuel-can, unlit torch"<<endl;
     cout<<"\nwhat would you like to do first?"<<endl;
     cout<<"\n1.\tExamine the rug"<<endl;
     cout<<"2.\tTake lighter"<<endl;
     cout<<"3.\tGrab fuel"<<endl;
-    cout<<"4.\tTake club with cloth wrapped around it"<<endl;
+    cout<<"4.\tUnlit torch"<<endl;
+    int input;
+    cin>>input;
+
+    int option1[4] = {1,2,3,4};
+    if(input == option1[0]){
+        cout<<"\nYou examine the rug. You notice a trap door underneath the rug"<<endl;
+        cout<<"You immediately remove the rug"<<endl;
+    }else if(input == option1[1]){
+        cout<<"You examine the lighter. Its brand new. You placed it in your pockets"<<endl;
+    } else if(input == option1[2]) {
+        cout<<"You examine the fuel. You notice that you can use this with the lighter. Its now in your pockets."<<endl;
+    }else if(input == option1[3]) {
+        cout<<"You examine examine unlit torch, and see you have items to can light it up."<<endl;
+    }
 }
 //main body-------------------------------------------------------------------------------
 int main () {
