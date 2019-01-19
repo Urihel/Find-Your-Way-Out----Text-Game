fc440c0af1d1c7747aaab0f59a18938b6db44c76
Urihel Coleman <45701213+Urihel@users.noreply.github.com>
2019-01-18 19:29:28 -0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,8 +2,22 @@
 
 using namespace std;
 
+void stairCase(){
+    cout<<"\nYou walk up the stairs. As you walk you notice the lights are flickering. You continue up anyway. When \nyou reach the top, there is a room facing you with a sign saying keep out!" <<endl;
+    cout<<"\nWhat would you like to do?"<<endl;
+    cout<<"1.\tOpen the door"<<endl;
+    cout<<"2.\tHead back downstairs"<<endl;
+    int input;
+    cin>>input;
 
-
+    if(input == 1) {
+        cout<<"You open the door and find a key on the floor."<<endl;
+        cout<<"Do you want to pick up the key y/n ?"<<endl;
+        cout<<"1.\tYes"<<endl;
+        cout<<"2.\tNo"<<endl;
+    }
+}
+//--------------------------------------
 void gameMenu() {
     cout<<"\n1.\tNew Game"<<endl;
 }
@@ -28,11 +42,8 @@ void gameText() {
 }
 //--------------------------------------
 void entrance() {
-    cout<<"\nYou found your way into an abandoned house."<<endl;
-    cout<<"Right in front of you is a hallway which leads \nstraight ahead, a staircase to 2nd floor on the \nright and a door to a room to left"<<endl;
-
-    cout<<"\nType a number with keypad to choose path: "<<endl;
-
+    cout<<"\nYou've stumbled into an abandoned house. Upon Entering, the doors slams shut. A voice in the background \nwhispers \"There is still a way out\" as it fades away. You\'re now facing forward. In front of you, there \nis a long hallway. You also have a staircase which leads to the second floor to your right, and on the \nleft is room with a door"<<endl;
+    cout<<"\nWhat's going to happen first? "<<endl;
     cout<<"\n1.\tGo down hallway"<<endl;
     cout<<"2.\tTake staircase to second floor"<<endl;
     cout<<"3.\tOpen door on the left"<<endl;
@@ -41,11 +52,11 @@ void entrance() {
     cin>>input;
 
     int num[3] = {1,2,3};
-    //cout<<num[0]"the array is workring<<endl;
     if(input == num[0]) {
         cout<<"\nYou choose 1 and decided to go down the hallway."<<endl;
     } else if(input == num[1]) {
         cout<<"You choose 2 and decide to take the staircase."<<endl;
+        stairCase();
     } else if(input ==num[2]) {
         cout<<"Your chose 3 and decided to take the door to the left of you"<<endl;
     }
@@ -54,6 +65,7 @@ void entrance() {
 }
 //------------------------------------------------
 void hallway() {
+    entrance();
     cout<<"\nYou walk down the hallway and see a table in the middle with a lighter, fuel, and an unlit torch"<<endl;
     cout<<"Under the table there is a rug."<<endl;
     cout<<"\nwhat would you like to do first?"<<endl;
@@ -75,12 +87,14 @@ void hallway() {
     }else if(input == option1[3]) {
         cout<<"You examine examine unlit torch, and see you have items to can light it up."<<endl;
     }
+
 }
+
 //main body-------------------------------------------------------------------------------
 int main () {
     gameText();
     startGame();
     entrance();
-    hallway();
+
 }
 
